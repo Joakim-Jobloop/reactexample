95344011bd03e690012f51d0b41237f92e3e205b
JoakimJobloop <Joakim@jobloop.no>
2023-10-20 09:33:42 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import { useEffect } from 'react'
+import './App.css'
+import CATDOG from "/catHotdog.png"
+
+function App() {
+
+
+  return (
+    <>
+<nav>
+<p>nav</p>
+</nav>
+<main>
+<img src={CATDOG} alt=""/>
+</main>
+<footer>
+<p>footer</p>
+</footer>
+    </>
+  )
+}
+
+export default App
